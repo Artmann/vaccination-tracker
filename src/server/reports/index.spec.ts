ee2be392c717ec069d160731773bbaf9afb884be
Christoffer Artmann <Artgaard@gmail.com>
2021-06-12 21:37:54 +0200

    refactor: Store reports in a database.

--- src/server/reports/index.spec.ts
+++ src/server/reports/index.spec.ts
@@ -1,9 +1,28 @@
 import request from 'supertest';
 
 import app from '../app';
+import { recreateDatabase } from '../database';
+import { Report } from './report';
 
 describe('/reports', () => {
+  beforeEach(async() => {
+    await recreateDatabase();
+  });
+
   it('returns a list of reports', async () => {
+    await Report.create({
+      numberOfDoses: 88,
+      timestamp: 1622788451000
+    });
+    await Report.create({
+      numberOfDoses: 122,
+      timestamp: 1622897672000
+    });
+    await Report.create({
+      numberOfDoses: 56,
+      timestamp: 1623075163000
+    });
+
     const server = app.listen();
     const response = await request(server)
       .get('/api/reports');
@@ -12,34 +31,25 @@ describe('/reports', () => {
     expect(response.body).toEqual({
       reports: [
         {
+          createdAt: expect.any(String),
           id: 1,
           numberOfDoses: 88,
-          timestamp: 1622788451000
+          timestamp: 1622788451000,
+          updatedAt: expect.any(String)
         },
         {
+          createdAt: expect.any(String),
           id: 2,
           numberOfDoses: 122,
-          timestamp: 1622897672000
+          timestamp: 1622897672000,
+          updatedAt: expect.any(String)
         },
         {
+          createdAt: expect.any(String),
           id: 3,
           numberOfDoses: 56,
-          timestamp: 1623075163000
-        },
-        {
-          id: 4,
-          numberOfDoses: 67,
-          timestamp: 1623062624000
-        },
-        {
-          id: 5,
-          numberOfDoses: 30,
-          timestamp: 1623135203000
-        },
-        {
-          id: 6,
-          numberOfDoses: 128,
-          timestamp: 1623143494000
+          timestamp: 1623075163000,
+          updatedAt: expect.any(String)
         }
       ]
     });
