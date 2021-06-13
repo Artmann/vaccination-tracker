063fc9bbb5dd1dcfac565b11e2671ed68aed2826
Christoffer Artmann <Artgaard@gmail.com>
2021-06-13 15:17:53 +0200

    feat: Add an API endpoint for creating reports.

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,13 @@
 import { promises as fs } from 'fs';
 import Koa, { Context } from 'koa';
+import koaBody  from 'koa-body';
 import koaLogger from 'koa-logger';
 import mount from 'koa-mount';
 import Router from 'koa-router';
 import serve from 'koa-static';
 import { join } from 'path';
 
-import { reportsRoute } from './reports';
+import { createReportRoute, reportsRoute } from './reports';
 
 const app = new Koa();
 const router = new Router();
@@ -18,6 +19,7 @@ const staticsPath = process.env['NODE_ENV'] === 'production'
 const statics = serve(staticsPath);
 
 router.get('/api/reports', reportsRoute);
+router.post('/api/reports', createReportRoute);
 
 router.get('/*', async function(context: Context) {
   const template = await fs.readFile(join(staticsPath, 'index.html'),  'utf-8');
@@ -26,6 +28,7 @@ router.get('/*', async function(context: Context) {
 });
 
 app
+  .use(koaBody())
   .use(mount('/statics', statics))
   .use(router.routes())
   .use(router.allowedMethods())
--- src/server/reports/index.spec.ts
+++ src/server/reports/index.spec.ts
@@ -4,56 +4,122 @@ import app from '../app';
 import { recreateDatabase } from '../database';
 import { Report } from './report';
 
-describe('/reports', () => {
+describe('Reports', () => {
   beforeEach(async() => {
     await recreateDatabase();
   });
 
-  it('returns a list of reports', async () => {
-    await Report.create({
-      numberOfDoses: 88,
-      timestamp: 1622788451000
-    });
-    await Report.create({
-      numberOfDoses: 122,
-      timestamp: 1622897672000
-    });
-    await Report.create({
-      numberOfDoses: 56,
-      timestamp: 1623075163000
+  describe('GET /reports', () => {
+
+    it('returns a list of reports', async () => {
+      await Report.create({
+        numberOfDoses: 88,
+        timestamp: 1622788451000
+      });
+      await Report.create({
+        numberOfDoses: 122,
+        timestamp: 1622897672000
+      });
+      await Report.create({
+        numberOfDoses: 56,
+        timestamp: 1623075163000
+      });
+
+      const server = app.listen();
+      const response = await request(server)
+        .get('/api/reports');
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual({
+        reports: [
+          {
+            createdAt: expect.any(String),
+            id: 1,
+            numberOfDoses: 88,
+            timestamp: 1622788451000,
+            updatedAt: expect.any(String)
+          },
+          {
+            createdAt: expect.any(String),
+            id: 2,
+            numberOfDoses: 122,
+            timestamp: 1622897672000,
+            updatedAt: expect.any(String)
+          },
+          {
+            createdAt: expect.any(String),
+            id: 3,
+            numberOfDoses: 56,
+            timestamp: 1623075163000,
+            updatedAt: expect.any(String)
+          }
+        ]
+      });
+
+      server.close();
     });
 
-    const server = app.listen();
-    const response = await request(server)
-      .get('/api/reports');
+  });
 
-    expect(response.status).toEqual(200);
-    expect(response.body).toEqual({
-      reports: [
-        {
-          createdAt: expect.any(String),
-          id: 1,
-          numberOfDoses: 88,
-          timestamp: 1622788451000,
-          updatedAt: expect.any(String)
-        },
-        {
-          createdAt: expect.any(String),
-          id: 2,
-          numberOfDoses: 122,
-          timestamp: 1622897672000,
-          updatedAt: expect.any(String)
-        },
-        {
-          createdAt: expect.any(String),
-          id: 3,
-          numberOfDoses: 56,
-          timestamp: 1623075163000,
-          updatedAt: expect.any(String)
-        }
-      ]
+  describe('POST /reports', () => {
+
+    it('creates a report', async() => {
+      const server = app.listen();
+
+      const response = await request(server)
+        .post('/api/reports')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send(JSON.stringify({
+          numberOfDoses: 300,
+          timestamp: 1623589468270
+        }))
+        .expect(200);
+
+      expect(response.body).toEqual({
+        createdAt: expect.any(String),
+        id: expect.any(Number),
+        numberOfDoses: 300,
+        timestamp: 1623589468270,
+        updatedAt: expect.any(String)
+      });
+
+      server.close();
     });
 
-    server.close();
+    it('requries numberOfDoses as a parameter', async() => {
+      const server = app.listen();
+
+      const response = await request(server)
+        .post('/api/reports')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send(JSON.stringify({
+          timestamp: 1623589468270
+        }))
+        .expect(400);
+
+      expect(response.text).toEqual('numberOfDoses is required.');
+
+      server.close();
+    });
+
+    it('requries timestamp as a parameter', async() => {
+      const server = app.listen();
+
+      const response = await request(server)
+        .post('/api/reports')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send(JSON.stringify({
+          numberOfDoses: 231
+        }))
+        .expect(400);
+
+      expect(response.text).toEqual('timestamp is required.');
+
+      server.close();
+    });
+
   });
 });
